8411bf4c7c8da810532151f998f93df9bab0ff3e
Maheshp <maheshp.ug21.ee@nitp.ac.in>
2024-12-20 23:05:08 +0530

    Changes few things

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
 
-        <NavBar />
+        <NavBar/>
       <DataTableComponent/>
 
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,8 @@
+import React from "react";
 const NavBar = () => {
     return (
-        <>
-            <div className="h-[10vh] w-full flex justify-between p-[3vh] bg-[#0f172a] text-white">
-                <h1 className="font-bold text-2xl cursor-pointer -tracking-tight">GrowMeOrganic</h1>
-                <div className="flex gap-[3vw] font-light text-xl cursor-pointer">
-                    <h1>Home</h1>
-                    <h1>About</h1>
-                    <h1>Help</h1>
-                </div>
-
-            </div>
-        </>
-    )
+        <div><h1 className="justify-center text-red-600">GrowMeOrganic</h1></div>
+    );
 }
+
 export default NavBar;
